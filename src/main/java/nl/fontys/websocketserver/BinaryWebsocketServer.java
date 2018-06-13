a184e176ee84051270c90045b0d601086de50284
ruudhuijts <r.huijts@fontys.nl>
2018-06-13 14:41:04 +0200

    initial commit

---
+++ src/main/java/nl/fontys/websocketserver/BinaryWebsocketServer.java
@@ -0,0 +1,44 @@
+package nl.fontys.websocketserver;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@ServerEndpoint("/images")
+public class BinaryWebsocketServer {
+
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnOpen
+    public void onOpen(Session session){
+        System.out.println(session.getId() + " connected!");
+        sessions.add(session);
+    }
+
+    @OnClose
+    public void onClose(Session session){
+        sessions.remove(session);
+    }
+
+    @OnError
+    public void onError(Throwable t){
+        t.printStackTrace();
+    }
+
+    @OnMessage
+    public void onMessage(ByteBuffer byteBuffer){
+        for(Session session : sessions){
+            try{
+                session.getBasicRemote().sendBinary(byteBuffer);
+            }catch (IOException e){
+                Logger.getLogger(BinaryWebsocketServer.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+}
